c491c435aef2a74f3767d24d7b37530edab69fc0
Jonathan Gruber <gruberjonathan@gmail.com>
2021-05-18 19:32:19 +0200

    Add noopener and noreferrer attributes

--- js/script.js
+++ js/script.js
@@ -2,26 +2,25 @@
  * Parse Google Docs files to retrieve their edit link and redirect to it
  */
 
-const GOOGLE_DOCS_EXTENSIONS = ['.gddoc', '.gdlink', '.gdsheets', '.gdslides'];
+const GOOGLE_DOCS_EXTENSIONS = [".gddoc", ".gdlink", ".gdsheets", ".gdslides"];
 
-$('#fileList').click((event) => {
-  const fileLink = $(event.target).closest('a.name');
-  const extension = fileLink.find('.extension').text();
+$("#fileList").click((event) => {
+  const fileLink = $(event.target).closest("a.name");
+  const extension = fileLink.find(".extension").text();
 
-  if (!GOOGLE_DOCS_EXTENSIONS.includes(extension) || !fileLink.attr('href')) {
+  if (!GOOGLE_DOCS_EXTENSIONS.includes(extension) || !fileLink.attr("href")) {
     return;
   }
 
   event.preventDefault();
   event.stopPropagation();
 
-  fetch(fileLink.attr('href'))
-    .then(response => response.json())
-    .then(gdoc => {
+  fetch(fileLink.attr("href"))
+    .then((response) => response.json())
+    .then((gdoc) => {
       if (gdoc.url) {
-        window.open(gdoc.url, '_blank').focus();
+        window.open(gdoc.url, "_blank", "noopener,noreferrer").focus();
       }
     })
     .catch(console.error);
-})
-
+});
